fb3c9dea5290102b71275ff983cdc35d2fb6057c
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-03-13 23:02:53 +0530

    Sign-up-completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -12,14 +13,26 @@ export class AuthService {
         // generate the password..
         const hash = await argon.hash(dto.password)
         // save the new user in Db...
-        const user = await this.prisma.user.create({ 
-            data: { 
-                email: dto.email,
-                hash
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+
+            delete user.hash;
+            // return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException("Credentials not available!")
+                }
             }
-        })
-        // return the saved user
-        return user;
+            throw error;
+            
+        }
     }
     
     signin(){ 
